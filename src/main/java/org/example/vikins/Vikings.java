f775370fc08d3f5bbb43dd0e63fb03a0cc5e3012
LuLu <lu.luzan@gmail.com>
2023-07-14 12:26:56 +0200

    demo

--- src/main/java/org/example/vikins/Vikings.java
+++ src/main/java/org/example/vikins/Vikings.java
@@ -1,4 +1,28 @@
 package org.example.vikins;
 
-public class Vikings {
+import org.example.soldier.Soldier;
+
+public class Vikings extends Soldier {
+    public String name;
+
+    public Vikings(int strength, int health, String name) {
+        super(strength,health);
+        this.name = name;
+    }
+    public void receiveDamage(int damage) {
+        super.health -= damage;
+    }
+
+    public String getMsg() {
+        if (super.health <= 0) {
+            return name + " it's fucking dead";
+        } else {
+            return name + " has received " + super.health + " points of damage";
+        }
+    }
+
+    public String battleCry() {
+        return "Odin Owns You All!";
+    }
 }
+
