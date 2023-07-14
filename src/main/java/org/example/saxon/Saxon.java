cff21cd0646bb518860f496dbfb3be3e8351077c
LuLu <lu.luzan@gmail.com>
2023-07-14 12:26:56 +0200

    soldier_done

--- src/main/java/org/example/saxon/Saxon.java
+++ src/main/java/org/example/saxon/Saxon.java
@@ -1,4 +1,21 @@
 package org.example.saxon;
 
-public class Saxon {
+
+import org.example.soldier.Soldier;
+import org.example.vikins.Vikings;
+
+public class Saxon extends Soldier {
+    public Saxon(int strength, int health) {
+        super(strength, health);
+    }
+    public int attack() {
+        return super.strength;
+    }
+    public String getMsg() {
+        if (super.health <= 0) {
+            return "A Saxon has died in combat";
+        } else {
+            return "A Saxon has received " + super.health + " points of damage";
+        }
+    }
 }
--- src/main/java/org/example/soldier/Soldier.java
+++ src/main/java/org/example/soldier/Soldier.java
@@ -1,4 +1,34 @@
 package org.example.soldier;
 
-public class Soldier {
-}
+    public class Soldier {
+        public int health;
+        public int strength;
+
+        public Soldier(int health, int strength) {
+            this.health = health;
+            this.strength = strength;
+        }
+//COMO ES PUBLICO NO NECESITAMOS LOS GETTERS NOR SETTERS
+//        public int getHealth() {
+//            return health;
+//        }
+//
+//        public int getStrength() {
+//            return strength;
+//        }
+//
+//        public void setHealth(int health) {
+//            this.health = health;
+//        }
+//
+//        public void setStrength(int strength) {
+//            this.strength = strength;
+//        }
+        public int attack() { // Aplicamos daño y devolvemos la fuerza de soldier
+            return this.strength;
+        }
+
+        public void receiveDamage(int damage) {
+            health -= damage;
+        }
+    }
